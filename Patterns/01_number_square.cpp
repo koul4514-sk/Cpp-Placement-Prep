156833ff42e1131907fe1003e2469913e3340c0c
koul4514-sk <koul4514@gmail.com>
2026-01-08 21:28:56 +0530

    Create 01_number_square.cpp

---
+++ Patterns/01_number_square.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+   
+    int count=0;
+    int i=1;
+    while(i<=3){
+         int j=1;
+         
+        while(j<=3){
+            count++;
+            cout<<count<<" ";
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+        
+        
+    }
+    
+
+    return 0;
+
+}

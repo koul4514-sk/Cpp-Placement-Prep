273ee1d8bce39d2601751a910a36820458674a3b
Saksham Koul Babu <koul4514@gmail.com>
2026-01-09 20:44:52 +0530

    Create 02_star_right_triangle.cpp

---
+++ 02_star_right_triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i =1;
+    while(i<=4){
+        int j=1;
+      // For pattern i=number of stars.
+        while(j<=i){
+            cout<<"* ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+
+
+    return 0;
+}
